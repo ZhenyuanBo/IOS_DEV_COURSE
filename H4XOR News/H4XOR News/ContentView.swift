cc94b2b3f644884075b260c8e431e59cd215a535
Zhenyuan Bo <bzyutoronto@gmail.com>
2020-10-22 10:58:44 -0400

    commit

--- H4XOR News/H4XOR News/ContentView.swift
+++ H4XOR News/H4XOR News/ContentView.swift
@@ -1,21 +1,39 @@
 //
 //  ContentView.swift
-//  H4XOR NEWS
+//  H4XOR News
 //
-//  Created by Zhenyuan Bo on 2020-10-22.
+//  Created by Zhenyuan Bo on 2020-10-21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List(posts) {(post) in
+                Text(post.title)
+            }
+            .navigationBarTitle("H4XOR NEWS")
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+//for a post to be identifiable, it has to have id
+struct Post: Identifiable{
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola")
+]
+
--- H4XOR News/H4XOR News/Models/NetworkManager.swift
+++ H4XOR News/H4XOR News/Models/NetworkManager.swift
@@ -1,8 +1,14 @@
-//
-//  NetworkManager.swift
-//  H4XOR NEWS
-//
-//  Created by Zhenyuan Bo on 2020-10-22.
-//
-
 import Foundation
+
+class NetworkManager{
+    func fetchData(){
+        if let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page"){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error == nil{
+                    let decoder = JSONDecoder(
+                }
+            }
+        }
+    }
+}
