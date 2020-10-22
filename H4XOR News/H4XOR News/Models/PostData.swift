971fca9153638c71561a7db894007a9c1340ca39
Zhenyuan Bo <bzyutoronto@gmail.com>
2020-10-22 12:50:06 -0400

    commit

--- H4XOR News/H4XOR News/Models/PostData.swift
+++ H4XOR News/H4XOR News/Models/PostData.swift
@@ -1,8 +1,16 @@
-//
-//  PostData.swift
-//  H4XOR NEWS
-//
-//  Created by Zhenyuan Bo on 2020-10-22.
-//
-
 import Foundation
+
+struct Results: Decodable{
+    let hits: [Post]
+}
+
+
+struct Post: Decodable, Identifiable{
+    var id: String{
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
