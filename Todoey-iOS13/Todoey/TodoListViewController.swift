b0227fcd243076b73fe848985a2e0236631046f7
Zhenyuan Bo <bzyutoronto@gmail.com>
2020-10-25 12:32:07 -0400

    commit

--- Todoey-iOS13/Todoey/TodoListViewController.swift
+++ Todoey-iOS13/Todoey/TodoListViewController.swift
@@ -44,7 +44,10 @@ class TodoListViewController: UITableViewController {
         //create alert
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+            DispatchQueue.main.async {
+                self.itemArray.append(textField.text!)
+                self.tableView.reloadData()
+            }
         }
         
         alert.addTextField { (alertTextField) in
