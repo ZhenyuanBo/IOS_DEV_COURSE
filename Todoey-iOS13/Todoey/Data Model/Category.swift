6c3b16eb279c4e079edc06c58e88247232656c74
Zhenyuan Bo <bzyutoronto@gmail.com>
2020-10-29 23:13:52 -0400

    commit

---
+++ Todoey-iOS13/Todoey/Data Model/Category.swift
@@ -0,0 +1,9 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Zhenyuan Bo on 2020-10-29.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Todoey-iOS13/Todoey/Data Model/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Zhenyuan Bo on 2020-10-29.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
